72ad71182b21c256c861ebc288b30d28147f81a0
JeffDDN <rodriguesjefferson990@gmail.com>
2024-09-23 23:13:18 -0300

    first commit

---
+++ src/main/java/com/darksmusic/darks_music/DarksMusicApplication.java
@@ -0,0 +1,13 @@
+package com.darksmusic.darks_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarksMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarksMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darksmusic/darks_music/DarksMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darksmusic.darks_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarksMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
